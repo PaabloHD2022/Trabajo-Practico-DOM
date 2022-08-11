ae5772e6b794a951e76046d82bc152bd64171403
PaabloHD2022 <pablodemartini@gmail.com>
2022-08-11 00:00:40 -0300

    Ultimos arreglos - Me funciona en la PC

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 
 //Generamos arreglo vacio para guardar los datos.
 let nuevoProducto = new Array();
+
 let alerts = document.getElementById("alerts"); // Alerta por cargar mal o no cargar un dato.
 let prodCtn = document.getElementById("prodCtn"); // Ventana donde aparece el nuevo producto cargado con sus datos y foto.
 let btnNuevoProd = document.getElementById("btnNuevoProd"); // Boton para cargar datos.
+
 btnNuevoProd.addEventListener("click", ()=>{ //Se pasan dos parametros, el evento y que quiero hacer cuando aprieto el boton que en este caso es validar los datos y generar el nuevo producto con la funcion.
+    
     if (validar_datos()){ //Funcion validar datos sirve para validar la info ingresada por el usuario. Esta creada mas abajo.
+        
         crearProducto(); //Si los datos son validos se genera un nuevo producto que se guardan en generar producto nuevo.
     }   
 });
@@ -20,32 +24,39 @@ function validar_datos(){
         let input_cantMin = document.getElementById("cantMin").value;
         let input_precio = document.getElementById("precio").value;
         let input_imgProd = document.getElementById("imgProd").value;
+        
         let alerts_mensajes = new Array();
     
         if (input_codProd < 0 || input_codProd == isNaN || !input_codProd){
             alerts_mensajes.push("Ingrese un codigo de producto");
         }
+        
         if(input_item =""){
             alerts_mensajes.push("ingrese un item");
         }
+        
         if (input_descripcion =""){
             alerts_mensajes.push("ingrese un item");
         }
+        
         if (input_cantMin < 0 || input_cantMin == isNaN || !input_cantMin){
             alerts_mensajes.push("Ingrese cantidad de venta minima de este producto");
         }
+        
         if ( input_precio < 0 || input_precio == isNaN || !input_precio){
             alerts_mensajes.push("Ingrese precio del producto");
         }
+        
         if (input_imgProd =""){
             alerts_mensajes.push("Ingrese la foto del producto");
         }
         
-    /* switch(validar_datos){
-        case "input_codProd":
+        
+        /* switch(validar_datos){
+        
+            case "input_codProd":
             input_codProd < 0 || isNaN || !input_codProd;
-            alerts_mensajes.push("Ingrese un codigo de producto");
-            
+            alerts_mensajes.push("Ingrese un codigo de producto");            
         
         case "input_item":
             input_item ="";
@@ -69,15 +80,16 @@ function validar_datos(){
             
     } */
         
-    if (alerts_mensajes.length > 0){
+        if (alerts_mensajes.length > 0){
         
         let lista_errores = document.createElement("ul");
         lista_errores.textContent = "Error en la carga de datos: ";
+        
         alerts_mensajes.forEach(mensaje =>{
             lista_errores.appendChild(crear_li(mensaje));
         })
 
-    alerts.appendChild(lista_errores);
+        alerts.appendChild(lista_errores);
 
         return false;
 
@@ -119,13 +131,16 @@ function generar_nuevo_producto(producto){
     new_img.src = producto.imgProd;
     new_div.appendChild(new_h2);
     new_div.appendChild(new_img);
+    
     let prodActiv = document.createElement("h4");
     prodActiv.textContent = producto.prodActiv;
     new_div.appendChild(prodActiv);
+    
     let contenedor = document.getElementById("prodCtn");
     contenedor.appendChild(new_div);
     resetear_form();
 }
+
 function resetear_form(){
     document.getElementById("codProd").value = "";
     document.getElementById("item").value = "";
